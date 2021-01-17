0f14bd9fee2576945455c4e4948934173229e390
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2021-01-17 22:46:30 +0530

    added 8b and 10

---
+++ Lab08/lab08bcd.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Program 8 B,C,D</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+<form action="lab08bcd.php" method="post">
+	<table>
+		<tr><h5>Matrix 1</h5></tr>
+		<tr>
+			<td>Enter [0,0]:</td>
+			<td><input type="text" name="M100" required /></td>
+		</tr>
+		<tr>
+			<td>Enter [0,1]:</td>
+			<td><input type="text" name="M101" required></td>
+		</tr>
+		<tr>
+			<td>Enter [1,0]:</td>
+			<td><input type="text" name="M110" required></td>
+		</tr>
+		<tr>
+			<td>Enter [1,1]:</td>
+			<td><input type="text" name="M111" required></td>
+		</tr>
+	</table>
+	<table>
+		<tr><h5>Matrix 2</h5></tr>
+		<tr>
+			<td>Enter [0,0]:</td>
+			<td><input type="text" name="M200" required /></td>
+		</tr>
+		<tr>
+			<td>Enter [0,1]:</td>
+			<td><input type="text" name="M201" required></td>
+		</tr>
+		<tr>
+			<td>Enter [1,0]:</td>
+			<td><input type="text" name="M210" required></td>
+		</tr>
+		<tr>
+			<td>Enter [1,1]:</td>
+			<td><input type="text" name="M211" required></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" name="pop" value="Calculate"></td>
+		</tr>
+	</table>
+</form>
+
+<?php 
+	if(isset($_POST["pop"])) {
+		$M100 = $_POST["M100"];
+		$M101 = $_POST["M101"];
+		$M110 = $_POST["M110"];
+		$M111 = $_POST["M111"];
+		$M200 = $_POST["M200"];
+		$M201 = $_POST["M201"];
+		$M210 = $_POST["M210"];
+		$M211 = $_POST["M211"];
+
+		$matrix1 = array(array($M100, $M101), array($M110, $M111));
+		$matrix2 = array(array($M200, $M201), array($M210, $M211));
+		$rowCount1 = count($matrix1);
+		$colCount1 = count($matrix1[0]);
+		$rowCount2 = count($matrix2);
+		$colCount2 = count($matrix2[0]);
+
+		// show the entered matrix
+		echo "<hr>";
+		echo "<h4>Matrix 1</h4>";
+		for ($r=0; $r < $rowCount1; $r++) { 
+			echo "</br>";
+			for ($c=0; $c < $colCount1; $c++) {
+				echo $matrix1[$r][$c]. " ";
+			}
+		}
+		echo "<h4>Matrix 2</h4>";
+		for ($r=0; $r < $rowCount2; $r++) { 
+			echo "</br>";
+			for ($c=0; $c < $colCount2; $c++) {
+				echo $matrix2[$r][$c]. " ";
+			}
+		}
+		echo "<hr>";
+
+		// sum of the matrix
+		$sum = "";
+		echo "<h4>The Sum of Matrix is:</h4>";
+		for ($r=0; $r < $rowCount1; $r++) {
+			echo "<br>";
+			for ($c=0; $c < $colCount1; $c++) { 
+				if(is_numeric($matrix1[$r][$c]) && is_numeric($matrix2[$r][$c])) {
+					$val = $matrix1[$r][$c] + $matrix2[$r][$c];
+					$sum = $sum . " " . $val;
+				} else {
+					echo "<h4>Enter numbers only</h4>";
+					break;
+				}
+			}
+			$sum = $sum . "<br>";
+		}
+		echo $sum;
+
+		// Transpose of matrix
+		echo "<hr>";
+		echo "<h4>Transpose of matrix1: </h4>";
+		for ($c=0; $c < $colCount1; $c++) {
+			echo "</br>";
+			for ($r=0; $r < $rowCount1; $r++) {
+				echo $matrix1[$r][$c] . " ";
+			}
+		}
+		echo "</br>";
+		echo "<h4>Transpose of matrix2: </h4>";
+		for ($c=0; $c < $colCount2; $c++) {
+			echo "</br>";
+			for ($r=0; $r < $rowCount2; $r++) {
+				echo $matrix2[$r][$c] . " ";
+			}
+		}
+		echo "<hr>";
+
+		// matrix multiplication
+		echo "<h4>Multiplication of matrix </h4>";
+		$result = array();
+		for ($i=0; $i < $rowCount1; $i++) { 
+			for ($j=0; $j < $colCount2; $j++) { 
+				$result[$i][$j] = 0;
+				for ($k=0; $k < $rowCount2; $k++) { 
+					$result[$i][$j] += $matrix1[$i][$k] * $matrix2[$k][$j];
+				}
+			}
+		}
+		//print_r($result);	// prints in a wierd form use for loop to display only content
+		for ($i=0; $i < $rowCount1; $i++) { 
+			echo "</br>";
+			for ($j=0; $j < $colCount2; $j++) { 
+				echo $result[$i][$j] . " ";
+			}
+		}
+		echo "<hr>";
+	}
+ ?>
+</body>
---
+++ Lab10/lab10.php
@@ -0,0 +1,92 @@
+<!--
+DATABASE SETUP
+1. Go to phpmyadmin
+2. Go to SQL tab
+3. Type the below code:
+CREATE DATABASE weblab;
+CREATE TABLE student(usn varchar(10),name varchar(20),address varchar(20));
+INSERT INTO `student` (`usn`, `name`, `address`) VALUES
+('1RG17CS300', 'QWE', 'QWEaddress'),
+('1RG17CS100', 'ABC', 'ABCaddress'),
+('1RG17CS200', 'XYZ', 'XYZaddress'),
+('1RG17CS250', 'MNO', 'MNOaddress');;
+4. DATABASE READY NOW TYPE THE PHP CODE IN PHP FILE prg10.php -->
+
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+    table, td, th {
+        border: 1px solid black;
+        width: 33%;
+        text-align: center;
+        border-collapse:collapse;
+        background-color:lightblue;
+    }
+    table {
+        margin: auto;
+    }
+</style>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "weblab";
+$a=[[]];
+$b=[[]];
+
+// Create connection
+// Opens a new connection to the MySQL server
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection and return an error description from the last connection error, if any
+if ($conn->connect_error)
+die("Connection failed: " . $conn->connect_error);
+
+$sql = "SELECT * FROM student";
+// performs a query against thedatabase
+$result = $conn->query($sql);
+echo "<br>";
+echo "<center> BEFORE SORTING</center>";
+echo "<table border='2'>";
+if($result) {
+    echo"<tr><th>USN</th><th>NAME</th><th>Address</th></tr>";
+    // output data of each row and fetches a result row as an associative array
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>". $row["usn"]."</td>";
+        echo "<td>". $row["name"]."</td>";
+        echo "<td>". $row["address"]."</td></tr>";
+        array_push($a,[$row["usn"],$row["name"],$row["address"]]);
+    }
+    foreach ($a as $key => $rowSORT) {
+    // replace 0 with the field's index/key
+        if($key!=0){
+            $b[$key] = $rowSORT[0];
+        }
+    }
+
+    array_multisort($b, SORT_ASC, $a);
+}
+else echo "<center style='color:red'> Table is Empty</center>";
+echo "</table>";
+echo "<br>";
+
+echo "<center> AFTER SORTING</center>";
+echo "<table border='2'>";
+if($result) {
+    echo"<tr><th>USN</th><th>NAME</th><th>Address</th></tr>";
+    $n=count($a);
+    for ( $i = 0 ; $i<($n-1) ; $i++) {
+        echo "<tr>";
+        echo "<td>". $a[$i][0]."</td>";
+        echo "<td>". $a[$i][1]."</td>";
+        echo "<td>". $a[$i][2]."</td>";
+        echo "</tr>";
+    }
+}
+else echo "<center style='color:red'> Table is Empty</center>";
+echo "</table>";
+$conn->close();
+?>
+</body>
+</html>
